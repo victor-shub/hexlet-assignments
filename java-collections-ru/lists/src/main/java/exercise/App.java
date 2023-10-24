cbb7fb7eee37e9edf4c406d628794077aadeff86
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-10-24 15:17:44 +0300

    do

---
+++ java-collections-ru/lists/src/main/java/exercise/App.java
@@ -0,0 +1,24 @@
+package exercise;
+
+import java.util.ArrayList;
+import java.util.List;
+
+// BEGIN
+public class App {
+    public static boolean scrabble(String lettersString, String word) {
+        char[] lettersArray = lettersString.toCharArray();
+        List<Character> lettersList = new ArrayList<>(lettersArray.length);
+        for (var letter: lettersArray) {
+            lettersList.add(letter);
+        }
+        char[] wordCharArray = word.toCharArray();
+        for (var wordLetter: wordCharArray) {
+            if (!lettersList.contains(wordLetter)) {
+                return false;
+            }
+            lettersList.remove(wordLetter);
+        }
+        return true;
+    }
+}
+//END
