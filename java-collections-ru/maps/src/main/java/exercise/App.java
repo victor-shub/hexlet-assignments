fc2c5c76b2b9966ab2adb27435c0583a5d2f7b71
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-10-24 17:21:06 +0300

    build base

---
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -0,0 +1,17 @@
+package exercise;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// BEGIN
+public class App {
+    public static Map<String, Integer> getWordCount(String sentence) {
+        Map<String, Integer> wordsCount = new HashMap<>();
+        return wordsCount;
+    }
+
+    public static String toString(Map<String, Integer> wordsCount) {
+        return "";
+    }
+}
+//END
