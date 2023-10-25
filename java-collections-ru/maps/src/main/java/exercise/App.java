b0d41ae33de43aa4b0ca8c85b6cae807d7a7dcf4
@hexlet/cli <support@hexlet.io>
2023-10-25 13:43:04 +0300

    submit java-collections-ru/maps

--- java-collections-ru/maps/src/main/java/exercise/App.java
+++ java-collections-ru/maps/src/main/java/exercise/App.java
@@ -8,30 +8,26 @@ public class App {
     public static Map<String, Integer> getWordCount(String sentence) {
         var workSentence = sentence.strip() + " ";
         Map<String, Integer> wordsCount = new HashMap<>();
-        System.out.println("wordsCount = " + wordsCount);
-        System.out.println("workSentence = " + workSentence);
         while (!workSentence.isEmpty()) {
             var spaceIndex = workSentence.indexOf(" ");
-            System.out.println("spaceIndex = " + spaceIndex);
             var word = workSentence.substring(0, spaceIndex);
-            System.out.println("word = " + word);
             if (wordsCount.containsKey(word)) {
                 var count = wordsCount.get(word);
-                System.out.println("count = " + count);
                 wordsCount.replace(word, count + 1);
             } else {
                 if (!word.isEmpty()) {
-                    System.out.println("count = 0");
                     wordsCount.put(word, 1);
                 }
             }
             workSentence = workSentence.substring(spaceIndex + 1);
-            System.out.println("workSentence = " + workSentence);
         }
         return wordsCount;
     }
 
     public static String toString(Map<String, Integer> map) {
+        if (map.isEmpty()) {
+            return "{}";
+        }
         var result = new StringBuilder();
         result.append("{\n");
         for (Map.Entry<String, Integer> keyValuePair : map.entrySet()) {
