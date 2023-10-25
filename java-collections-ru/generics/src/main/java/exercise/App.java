a8b4be0401b1c940e52cbdda8d6aecf97161c711
@hexlet/cli <support@hexlet.io>
2023-10-25 15:21:51 +0300

    submit java-collections-ru/generics

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -3,12 +3,17 @@ package exercise;
 import java.util.List;
 import java.util.Map;
 import java.util.ArrayList;
-import java.util.Map.Entry;
 
 // BEGIN
 class App {
     public static <K, V> List<Map<K, V>> findWhere(List<Map<K, V>> listOfMaps, Map<K, V> mapToFind) {
-
+        List<Map<K, V>> listOfMapsWhere = new ArrayList<>();
+        for (var map: listOfMaps) {
+            if (map.entrySet().containsAll(mapToFind.entrySet())) {
+                listOfMapsWhere.add(map);
+            }
+        }
+        return listOfMapsWhere;
     }
 }
 //END
