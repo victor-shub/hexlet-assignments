f0af041ec671dd4a43f5cd6fa9a01e985b7502d6
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-10-25 15:03:47 +0300

    add base

--- java-collections-ru/generics/src/main/java/exercise/App.java
+++ java-collections-ru/generics/src/main/java/exercise/App.java
@@ -6,5 +6,9 @@ import java.util.ArrayList;
 import java.util.Map.Entry;
 
 // BEGIN
+class App {
+    public static <K, V> List<Map<K, V>> findWhere(List<Map<K, V>> listOfMaps, Map<K, V> mapToFind) {
 
+    }
+}
 //END
