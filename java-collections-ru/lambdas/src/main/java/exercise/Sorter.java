8d710280917b5a6b958d82a1e8c6bb5a5bf72974
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-10-25 17:56:49 +0300

    build gradle

---
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -0,0 +1,11 @@
+package exercise;
+
+import java.util.Comparator;
+import java.util.Map;
+import java.util.List;
+import java.time.LocalDate;
+import java.util.stream.Collectors;
+
+// BEGIN
+
+// END
