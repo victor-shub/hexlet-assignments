8ea43f21c99f8f63ed2a3d63499e3b07172a2a39
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-10-25 17:12:01 +0300

    add base

---
+++ java-collections-ru/streams/src/main/java/exercise/App.java
@@ -0,0 +1,12 @@
+package exercise;
+
+import java.util.List;
+import java.util.Arrays;
+
+// BEGIN
+class App {
+    public static int getCountOfFreeEmails(List<String> emails) {
+        List<String> freeDomens = List.of("gmail.com", "yandex.ru", "hotmail.com");
+    }
+}
+// END
