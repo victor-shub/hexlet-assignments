1729fa0ee14e64931a4cfefabc2ec0136a4670a5
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-11-01 13:40:29 +0300

    something

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -1,11 +1,19 @@
 package exercise;
 
-import java.util.Comparator;
+import java.time.LocalDate;
 import java.util.Map;
 import java.util.List;
-import java.time.LocalDate;
 import java.util.stream.Collectors;
 
 // BEGIN
-
+class Sorter {
+    public static List<String> takeOldestMans(List<Map<String, String>> users) {
+        List<String> oldestMans = users.stream()
+                .filter(user -> user.get("gender").equals("male"))
+                .sorted((user1, user2) -> LocalDate.parse(user1.get("birthday")).compareTo(LocalDate.parse(user2.get("birthday")))
+                .map(user -> user.get("name"))
+                .collect(Collectors.toList()));
+        return oldestMans;
+    }
+}
 // END
