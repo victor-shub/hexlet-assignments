8b5572d4860a56779fbe17ec0a8e64e03076155f
@hexlet/cli <support@hexlet.io>
2023-11-01 17:08:53 +0300

    submit java-collections-ru/advanced-streams

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -6,10 +6,18 @@ import java.util.Arrays;
 // BEGIN
 class App {
     public static String getForwardedVariables(String confFileText) {
-        var envLines = confFileText.lines()
-                .filter(line -> line.startsWith("environment="))
-                .toArray();
-
+        var forwarder = "X_FORWARDED_";
+        var envCommand = "environment=\"";
+        String variables = confFileText.lines()
+                .filter(line -> line.startsWith(envCommand))
+                .map(line -> line.replace(envCommand, ""))
+                .map(line -> line.split(","))
+                .flatMap(Arrays::stream)
+                .filter(varString -> varString.startsWith(forwarder))
+                .map(varString -> varString.replace(forwarder, ""))
+                .map(varString -> varString.replace("\"", ""))
+                .collect(Collectors.joining(","));
+        return variables;
     }
 }
 //END
