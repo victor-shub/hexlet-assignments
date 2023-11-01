a9584dc05e4e510e0c2bb5130055bde21ce7c8a0
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-11-01 14:37:37 +0300

    create AppTest

--- java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
+++ java-collections-ru/lambdas/src/test/java/exercise/AppTest.java
@@ -4,5 +4,68 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 // BEGIN
+class AppTest {
+    @Test
+    void testAppWithEmptyArray() {
+        String[][] originArray = new String[0][];
+        String[][] actual = App.enlargeArrayImage(originArray);
+        var expected = originArray;
+        assertThat(actual).isEqualTo(expected);
+    }
 
+    @Test
+    void testAppWithOneLine() {
+        String[][] originArray = {{" ", "*", " ", "%", "*"}};
+        String[][] actual = App.enlargeArrayImage(originArray);
+        String[][] expected = {
+                {" ", " ", "*", "*", " ", " ", "%", "%", "*", "*"},
+                {" ", " ", "*", "*", " ", " ", "%", "%", "*", "*"}
+        };
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testAppWithOneColumn() {
+        String[][] originArray = {{"*"}, {"%"}, {" "}, {"*"}, {" "}};
+        String[][] actual = App.enlargeArrayImage(originArray);
+        String[][] expected = {
+                {"*", "*"},
+                {"*", "*"},
+                {"%", "%"},
+                {"%", "%"},
+                {" ", " "},
+                {" ", " "},
+                {"*", "*"},
+                {"*", "*"},
+                {" ", " "},
+                {" ", " "},
+        };
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void testApp() {
+        String[][] originArray = {
+                {"*", "%", "*", "*"},
+                {" ", "*", " ", "%"},
+                {" ", "%", "*", " "},
+                {"*", " ", " ", " "},
+                {" ", " ", "*", "*"}
+        };
+        String[][] actual = App.enlargeArrayImage(originArray);
+        String[][] expected = {
+                {"*", "*", "%", "%", "*", "*", "*", "*"},
+                {"*", "*", "%", "%", "*", "*", "*", "*"},
+                {" ", " ", "*", "*", " ", " ", "%", "%"},
+                {" ", " ", "*", "*", " ", " ", "%", "%"},
+                {" ", " ", "%", "%", "*", "*", " ", " "},
+                {" ", " ", "%", "%", "*", "*", " ", " "},
+                {"*", "*", " ", " ", " ", " ", " ", " "},
+                {"*", "*", " ", " ", " ", " ", " ", " "},
+                {" ", " ", " ", " ", "*", "*", "*", "*"},
+                {" ", " ", " ", " ", "*", "*", "*", "*"}
+        };
+        assertThat(actual).isEqualTo(expected);
+    }
+}
 // END
