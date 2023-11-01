827a29eb6e5e235e27081b468bf480cbd2ebed27
@hexlet/cli <support@hexlet.io>
2023-11-01 15:24:21 +0300

    submit java-collections-ru/lambdas

--- java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
+++ java-collections-ru/lambdas/src/main/java/exercise/Sorter.java
@@ -10,9 +10,13 @@ class Sorter {
     public static List<String> takeOldestMans(List<Map<String, String>> users) {
         List<String> oldestMans = users.stream()
                 .filter(user -> user.get("gender").equals("male"))
-                .sorted((user1, user2) -> LocalDate.parse(user1.get("birthday")).compareTo(LocalDate.parse(user2.get("birthday")))
+                .sorted((user1, user2) -> {
+                    LocalDate date1 = LocalDate.parse(user1.get("birthday"));
+                    LocalDate date2 = LocalDate.parse(user2.get("birthday"));
+                    return date1.compareTo(date2);
+                })
                 .map(user -> user.get("name"))
-                .collect(Collectors.toList()));
+                .collect(Collectors.toList());
         return oldestMans;
     }
 }
