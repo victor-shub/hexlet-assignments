8d8cd277296395d5391cc0336cc186d849119268
V. Shubina <victoriya.shubina.mex@gmail.com>
2023-11-03 14:29:12 +0300

    add base

--- java-collections-ru/other-collections/src/main/java/exercise/App.java
+++ java-collections-ru/other-collections/src/main/java/exercise/App.java
@@ -6,5 +6,9 @@ import java.util.Set;
 import java.util.TreeSet;
 
 // BEGIN
+class App {
+    public static LinkedHashMap<String, String> genDiff(Map<String, Object> map1, Map<String, Object> map2) {
 
+    }
+}
 //END
